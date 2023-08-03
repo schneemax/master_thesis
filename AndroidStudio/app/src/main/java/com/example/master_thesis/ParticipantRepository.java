03b0c571da0453b3d2c12b12db5899e008ac588d
Darmstadt <51033307+schneemax@users.noreply.github.com>
2023-08-03 13:51:35 +0200

    Create ParticipantRepository.java

---
+++ AndroidStudio/app/src/main/java/com/example/master_thesis/ParticipantRepository.java
@@ -0,0 +1,31 @@
+package com.example.master_thesis;
+
+public class ParticipantRepository {
+
+    private static ParticipantRepository sInstance;
+
+    private ParticipantRepository() {
+        //Constructor
+    }
+
+    ;
+
+    /**
+     * Singleton principle
+     */
+    public static ParticipantRepository getInstance() {
+        if (sInstance == null) {
+            synchronized (ParticipantRepository.class) {
+                if (sInstance == null) {
+                    sInstance = new ParticipantRepository();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    /**
+     * Get the information about participants.
+     */
+
+}
