8a4a65b72c7929432fda452575842b7d26a8d20a
Darmstadt <51033307+schneemax@users.noreply.github.com>
2023-08-08 17:20:57 +0200

    Update

--- AndroidStudio/app/src/main/java/com/example/master_thesis/ExperimentData.java
+++ AndroidStudio/app/src/main/java/com/example/master_thesis/ExperimentData.java
@@ -10,7 +10,8 @@ public class ExperimentData {
 
         ArrayList<String> steps = new ArrayList<String>();
         String groupAllocation = "Random";
-        String infoScreen = "Exploring the effects of chromatic light on plant growth, set up three groups of potted plants—Group A exposed to blue light, Group B to red light, and Group C to a combination of green and yellow light. Ensure all other environmental factors remain constant. Measure growth parameters such as height, leaf count, and color intensity over a span of four weeks. Analyze the data using ANOVA to determine if different light spectra significantly influence plant growth. This experiment aims to contribute insights into optimizing indoor horticulture techniques for improved yield and quality.";
+        //String infoScreen = "Exploring the effects of chromatic light on plant growth, set up three groups of potted plants—Group A exposed to blue light, Group B to red light, and Group C to a combination of green and yellow light. Ensure all other environmental factors remain constant. Measure growth parameters such as height, leaf count, and color intensity over a span of four weeks. Analyze the data using ANOVA to determine if different light spectra significantly influence plant growth. This experiment aims to contribute insights into optimizing indoor horticulture techniques for improved yield and quality.";
+        String infoScreen = "Thanks for participating! We will evaluate the results and send you a thank you gift.";
 
         steps.add("com.example.master_thesis.InfoScreenActivity");
         steps.add("com.example.master_thesis.ChooseTestSubjectActivity");
