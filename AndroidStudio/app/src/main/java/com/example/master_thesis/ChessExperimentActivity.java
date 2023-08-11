418183a4ab8f1bc4bbb3f43757e5879f51b4601c
Darmstadt <51033307+schneemax@users.noreply.github.com>
2023-08-11 16:49:00 +0200

    Update

--- AndroidStudio/app/src/main/java/com/example/master_thesis/ChessExperimentActivity.java
+++ AndroidStudio/app/src/main/java/com/example/master_thesis/ChessExperimentActivity.java
@@ -47,40 +47,7 @@ public class ChessExperimentActivity extends AppCompatActivity {
         });
 
         /*
-        // MockUp for data Output
-        File file = new File(Environment.getExternalStorageDirectory() + "/participant" + GetCurrentParticipantUseCase.getInstance().getCurrentParticipant() + "TimeData.csv");
-        try {
-            // create FileWriter object with file as parameter
-            FileWriter outputfile = new FileWriter(file);
 
-            // create CSVWriter object filewriter object as parameter
-            CSVWriter writer = new CSVWriter(outputfile);
-
-            // adding header to csv
-            String[] header = { "id", "time in milliseconds"};
-            writer.writeNext(header);
-
-            //Getting participant information
-            ArrayList<ParticipantEntity> participantEntities = GetParticipantDataUseCase.getInstance().getParticipantData();
-
-            //Writing current participant time into file
-            //String[] data = {String.valueOf(GetCurrentParticipantUseCase.getInstance().getCurrentParticipant()), String.valueOf(timeDifference)};
-
-
-            Iterator iter = participantEntities.iterator();
-            while (iter.hasNext()) {
-                String[] data = {String.valueOf(((ParticipantEntity)iter.next()).getId()), String.valueOf(((ParticipantEntity)iter.next()).getExperimentTime())};
-                writer.writeNext(data);
-            }
-            //closing writer connection
-            writer.close();
-        }
-        catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.out.println("Error");
-        }
-*/
 /*
         try {
             // Establish connection
